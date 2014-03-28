90cae81e516e205e254d4c353c7c66d233c218a1
Dan Ruscoe <danruscoe@gmail.com>
2014-03-28 11:11:01 -0700

    Added entity token generator javascript.

---
+++ modules/mailchimp_campaign/mailchimp_campaign.utils.js
@@ -0,0 +1,27 @@
+(function ($) {
+
+  Drupal.mailchimp_campaign_add_entity_token = function() {
+    console.log('Adding entity token.');
+
+    var entity_id = '';
+    var entity_value = $('#edit-content-entity-header-entity').val();
+    if (entity_value.length > 0) {
+      var entity_parts = entity_value.split(' ');
+      var entity_id_string = entity_parts[entity_parts.length - 1];
+
+      entity_id = entity_id_string.replace('[', '').replace(']', '');
+    }
+
+    var entity_type = $('#edit-content-entity-header-entity-type').val();
+    var view_mode = $('#edit-content-entity-header-entity-view-mode').val();
+
+    var token = '[mailchimp_campaign'
+      + '|entity_type=' + entity_type
+      + '|entity_id=' + entity_id
+      + '|view_mode=' + view_mode
+      + ']';
+
+    console.log(token);
+  };
+
+})(jQuery);
