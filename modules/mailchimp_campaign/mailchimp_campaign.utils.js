6d0b5eaa64b31613a2f0d1b14e63b66f0b9c0276
Dan Ruscoe <danruscoe@gmail.com>
2014-03-31 10:13:49 -0700

    Entity tokens may now be inserted into any point in a text field without overwriting data.

--- modules/mailchimp_campaign/mailchimp_campaign.utils.js
+++ modules/mailchimp_campaign/mailchimp_campaign.utils.js
@@ -1,18 +1,31 @@
 (function ($) {
 
+  /**
+   * Utility methods for MailChimp campaign management.
+   * Some text field manipulation code is adapted from
+   * the Tokens module.
+   */
   Drupal.behaviors.mailchimp_campaign_utils = {
     attach: function(context, settings) {
+      // Keep track of which textfield was last selected/focused.
+      $('textarea', context).focus(function() {
+        Drupal.settings.mailchimpCampaignFocusedField = this;
+        console.log('Got text field focus: ' + $(this).attr('id'));
+      });
+
       $('.add-entity-token', context).one('click', function() {
         var element_id = $(this).attr('id');
 
-        console.log(element_id);
+        // Determine form section using this element's ID.
         var element_id_parts = element_id.split('-');
         var section = element_id_parts[element_id_parts.length - 1];
 
-        var target_element_id = 'edit-content-html-' + section + '-value';
+        // Get the last selected text field.
+        var target_element = Drupal.settings.mailchimpCampaignFocusedField;
 
+        // Get the selected entity ID.
         var entity_id = '';
-        var entity_value = $('#edit-content-entity-header-entity').val();
+        var entity_value = $('#edit-content-entity-' + section + '-entity').val();
         if (entity_value.length > 0) {
           var entity_parts = entity_value.split(' ');
           var entity_id_string = entity_parts[entity_parts.length - 1];
@@ -20,6 +33,7 @@
           entity_id = entity_id_string.replace('[', '').replace(']', '');
         }
 
+        // Generate token based on user input.
         var entity_type = $('#edit-content-entity-' + section + '-entity-type').val();
         var view_mode = $('#edit-content-entity-' + section + '-entity-view-mode').val();
 
@@ -29,9 +43,31 @@
           + '|view_mode=' + view_mode
           + ']';
 
-        console.log(token);
+        // Insert token into last selected text field.
+        if (target_element) {
+          console.log('Inserting token: ' + token);
 
-        $('#' + target_element_id).val(token);
+          // IE support.
+          if (document.selection) {
+            target_element.focus();
+            sel = document.selection.createRange();
+            sel.text = token;
+          }
+
+          // MOZILLA/NETSCAPE support.
+          else if (target_element.selectionStart || target_element.selectionStart == '0') {
+            var startPos = target_element.selectionStart;
+            var endPos = target_element.selectionEnd;
+            target_element.value = target_element.value.substring(0, startPos)
+              + token
+              + target_element.value.substring(endPos, target_element.value.length);
+          } else {
+            target_element.value += token;
+          }
+        }
+        else {
+          console.log('Unable to insert token; no text field selected.');
+        }
       });
     }
   }
