0029b3666048dd2770a511744fe353025ffeeba2
Dan Ruscoe <danruscoe@gmail.com>
2014-04-02 09:31:45 -0700

    Merge tags can now be inserted into a template by clicking on them.

--- modules/mailchimp_campaign/mailchimp_campaign.utils.js
+++ modules/mailchimp_campaign/mailchimp_campaign.utils.js
@@ -49,25 +49,7 @@
         if (target_element) {
           console.log('Inserting token: ' + token);
 
-          $('#entity-import-tag-field').hide();
-
-          // IE support.
-          if (document.selection) {
-            target_element.focus();
-            sel = document.selection.createRange();
-            sel.text = token;
-          }
-
-          // MOZILLA/NETSCAPE support.
-          else if (target_element.selectionStart || target_element.selectionStart == '0') {
-            var startPos = target_element.selectionStart;
-            var endPos = target_element.selectionEnd;
-            target_element.value = target_element.value.substring(0, startPos)
-              + token
-              + target_element.value.substring(endPos, target_element.value.length);
-          } else {
-            target_element.value += token;
-          }
+          Drupal.behaviors.mailchimp_campaign_utils.addTokenToElement(target_element, token);
         }
         else {
           // Insert token into token field, where it can be manually copied
@@ -80,6 +62,48 @@
         // Unset last focused field.
         Drupal.settings.mailchimpCampaignFocusedField = null;
       });
+
+      $('.add-merge-var', context).unbind('click').bind('click', function() {
+        // Get the last selected text field.
+        var target_element = Drupal.settings.mailchimpCampaignFocusedField;
+
+        // Get the merge var.
+        var element_id = $(this).attr('id');
+        var merge_var = element_id.replace('merge-var-', '');
+        var token = '*|' + merge_var + '|*';
+
+        // Insert token into last selected text field.
+        if (target_element) {
+          console.log('Inserting token: ' + token);
+
+          $('#entity-import-tag-field').hide();
+
+          Drupal.behaviors.mailchimp_campaign_utils.addTokenToElement(target_element, token);
+        }
+
+        // Unset last focused field.
+        Drupal.settings.mailchimpCampaignFocusedField = null;
+      });
+    },
+
+    addTokenToElement: function(target_element, token) {
+      // IE support.
+      if (document.selection) {
+        target_element.focus();
+        sel = document.selection.createRange();
+        sel.text = token;
+      }
+
+      // MOZILLA/NETSCAPE support.
+      else if (target_element.selectionStart || target_element.selectionStart == '0') {
+        var startPos = target_element.selectionStart;
+        var endPos = target_element.selectionEnd;
+        target_element.value = target_element.value.substring(0, startPos)
+          + token
+          + target_element.value.substring(endPos, target_element.value.length);
+      } else {
+        target_element.value += token;
+      }
     }
   }
 
