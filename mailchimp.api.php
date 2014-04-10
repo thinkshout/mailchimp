2b90ebf756de106949bbdbaf895faf35e36bfcd7
Dan Ruscoe <danruscoe@gmail.com>
2014-04-10 11:20:29 -0700

    Coding standards / documentation updates.

--- mailchimp.api.php
+++ mailchimp.api.php
@@ -8,6 +8,10 @@
 /**
  * Alter mergevars before they are sent to Mailchimp.
  *
+ * @param array $mergevars
+ * @param object $entity
+ * @param string $entity_type
+ *
  * @return NULL
  */
 function hook_mailchimp_lists_mergevars_alter(&$mergevars, $entity, $entity_type) {
