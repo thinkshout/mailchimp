d083e1c80e4d2b447c14b11a24d8141cc51f9be9
Gabe Carleton-Barnes <gabe@thinkshout.com>
2015-03-18 16:01:23 -0700

    Scaffolding for mailchimp_signup entity.
    Based on https://www.drupal.org/node/1809494

---
+++ modules/mailchimp_signup/src/MailchimpSignupInterface.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\mailchimp_signup\MailchimpSignupInterface.
+ */
+namespace Drupal\mailchimp_signup;
+
+use Drupal\Core\Config\Entity\ConfigEntityInterface;
+
+interface MailchimpSignupInterface extends ConfigEntityInterface {
+  
+}
