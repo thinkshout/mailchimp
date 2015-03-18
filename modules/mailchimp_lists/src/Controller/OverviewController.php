3135cecd02c03458e10e0adddad6114b15fa69ef
Dan Ruscoe <danruscoe@gmail.com>
2015-03-18 13:29:12 -0700

    Added controller for lists overview page.

---
+++ modules/mailchimp_lists/src/Controller/OverviewController.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\mailchimp_lists\Controller\OverviewController.
+ */
+
+namespace Drupal\mailchimp_lists\Controller;
+
+use Drupal\Core\Controller\ControllerBase;
+use Drupal\Core\Routing\RouteMatchInterface;
+
+/**
+ * Mailchimp Lists overview controller.
+ */
+class OverviewController extends ControllerBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function content() {
+    $build = array(
+    );
+    return $build;
+  }
+
+}
