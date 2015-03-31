55df57db8e796f0d13c6ddbe77885644ce1e032d
Dan Ruscoe <danruscoe@gmail.com>
2015-03-31 15:45:18 -0700

    Functional lists overview page.

--- modules/mailchimp_lists/src/Controller/OverviewController.php
+++ modules/mailchimp_lists/src/Controller/OverviewController.php
@@ -9,6 +9,9 @@ namespace Drupal\mailchimp_lists\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
 use Drupal\Core\Routing\RouteMatchInterface;
+use Drupal\Core\Url;
+
+use Drupal\Component\Utility\String;
 
 /**
  * Mailchimp Lists overview controller.
@@ -19,9 +22,25 @@ class OverviewController extends ControllerBase {
    * {@inheritdoc}
    */
   public function content() {
+    $content = array();
+
+    $lists_admin_url = Url::fromUri('https://admin.mailchimp.com/lists/');
+
+    $lists_empty_message = t('You don\'t have any lists configured in your
+      MailChimp account, (or you haven\'t configured your API key correctly on
+      the Global Settings tab). Head over to !link and create some lists, then
+      come back here and click "Refresh lists from MailChimp"',
+      array('!link' => \Drupal::l(t('MailChimp'), $lists_admin_url)));
+
+    $content['lists_table'] = array(
+      '#type' => 'table',
+      '#header' => array(t('Name'), t('Members'), t('Webhook Status'),),
+      '#empty' => $lists_empty_message,
+    );
+
     $mc_lists = mailchimp_get_lists();
-    $rows = array();
     $webhook_url = mailchimp_webhook_url();
+
     foreach ($mc_lists as $mc_list) {
       $webhooks = mailchimp_webhook_get($mc_list['id']);
       $enabled = FALSE;
@@ -33,47 +52,44 @@ class OverviewController extends ControllerBase {
           }
         }
       }
+
+      $enable_url = Url::fromRoute('mailchimp_lists.webhook.enable', array('list_id' => $mc_list['id']));
+      $disable_url = Url::fromRoute('mailchimp_lists.webhook.disable', array('list_id' => $mc_list['id']));
+
       if ($enabled) {
-        // This is a webhook for this Mailchimp Module.
-        $webhook_status = "ENABLED (" . l(t('disable'), 'admin/config/services/mailchimp/lists/' . $mc_list['id'] . '/webhook/disable') . ')';
+        $webhook_status = "ENABLED (" . \Drupal::l(t('disable'), $disable_url) . ')';
       }
       else {
-        $webhook_status = "disabled (" . l(t('enable'), 'admin/config/services/mailchimp/lists/' . $mc_list['id'] . '/webhook/enable') . ')';
+        $webhook_status = "disabled (" . \Drupal::l(t('enable'), $enable_url) . ')';
       }
-      $rows[] = array(
-        l($mc_list['name'], 'https://admin.mailchimp.com/lists/dashboard/overview?id=' . $mc_list['web_id']),
-        $mc_list['stats']['member_count'],
-        $webhook_status,
+
+      $list_url = Url::fromUri('https://admin.mailchimp.com/lists/dashboard/overview?id=' . $mc_list['web_id']);
+
+      $content['lists_table'][$mc_list['id']]['name'] = array(
+        '#markup' => \Drupal::l($mc_list['name'], $list_url),
       );
-
+      $content['lists_table'][$mc_list['id']]['member_count'] = array(
+        '#markup' => $mc_list['stats']['member_count'],
+      );
+      $content['lists_table'][$mc_list['id']]['web_id'] = array(
+        '#markup' => $webhook_status,
+      );
     }
-    $table = array(
-      'header' => array(
-        t('Name'),
-        t('Members'),
-        t('Webhook Status'),
-      ),
-      'rows' => $rows,
+
+    $refresh_url = Url::fromRoute('mailchimp_lists.refresh', array('destination' => 'admin/config/services/mailchimp/lists'));
+
+    $content['refresh_link'] = array(
+      '#markup' => \Drupal::l(t('Refresh lists from Mailchimp'), $refresh_url),
     );
-    $refresh_link = l(t('Refresh lists from Mailchimp'),
-      'admin/config/services/mailchimp/list_cache_clear',
-      array('query' => array('destination' => 'admin/config/services/mailchimp/lists')));
-    if (empty($mc_lists)) {
-      $table['caption'] = $refresh_link;
-      drupal_set_message(t('You don\'t have any lists configured in your MailChimp account, (or you haven\'t configured your API key correctly on the Global Settings tab). Head over to !link and create some lists, then come back here and click "Refresh lists from MailChimp"',
-        array('!link' => l(t('MailChimp'), 'https://admin.mailchimp.com/lists/'))), 'warning');
-    }
-    else {
-      $options = 'Currently Available MailChimp lists:<i>';
-      foreach ($mc_lists as $mc_list) {
-        $options .= ' ' . $mc_list['name'] . ',';
-      }
-      $options = rtrim($options, ',');
-      $options .= ".</i><br />" . $refresh_link;
-      $table['caption'] = $options;
-    }
 
-    return theme('table', $table);
+    return $content;
+  }
+
+  /**
+   * Refreshes lists from MailChimp.
+   */
+  public function refresh() {
+    // TODO: Refresh lists from MailChimp.
   }
 
 }
