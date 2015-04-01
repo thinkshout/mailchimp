0a67406ac80aa55c7b7d4472417b26cd4136523a
Dan Ruscoe <danruscoe@gmail.com>
2015-04-01 13:54:02 -0700

    Added routing / tab for fields.

---
+++ modules/mailchimp_lists/src/Controller/MailchimpFieldsController.php
@@ -0,0 +1,32 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\mailchimp_lists\Controller\MailchimpFieldsController.
+ */
+
+namespace Drupal\mailchimp_lists\Controller;
+
+use Drupal\Core\Controller\ControllerBase;
+use Drupal\Core\Routing\RouteMatchInterface;
+use Drupal\Core\Url;
+
+use Drupal\Component\Utility\String;
+
+/**
+ * MailChimp Fields controller.
+ */
+class MailchimpFieldsController extends ControllerBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function overview() {
+    $content = array();
+
+
+
+    return $content;
+  }
+
+}
