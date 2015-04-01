3eb7a284bf611d2bb652ccbccf6ca6bffe7f2104
Dan Ruscoe <danruscoe@gmail.com>
2015-04-01 12:51:29 -0700

    Added webhook routing and controller.

---
+++ src/Controller/MailchimpWebhookController.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\mailchimp\Controller\MailchimpWebhookController.
+ */
+
+namespace Drupal\mailchimp\Controller;
+
+use Drupal\Core\Controller\ControllerBase;
+use Drupal\Core\Routing\RouteMatchInterface;
+use Drupal\Core\Url;
+
+/**
+ * MailChimp Webhook controller.
+ */
+class MailchimpWebhookController extends ControllerBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function endpoint($hash) {
+
+  }
+
+}
