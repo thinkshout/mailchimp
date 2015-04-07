78e3b6901af82ac448f5bf058160d697da98c16f
Dan Ruscoe <danruscoe@gmail.com>
2015-04-07 14:29:50 -0700

    Initial campaigns overview table.

--- modules/mailchimp_campaign/src/Controller/MailchimpCampaignController.php
+++ modules/mailchimp_campaign/src/Controller/MailchimpCampaignController.php
@@ -24,6 +24,71 @@ class MailchimpCampaignController extends ControllerBase {
   public function overview() {
     $content = array();
 
+    $content['campaigns_table'] = array(
+      '#type' => 'table',
+      '#header' => array(t('Title'), t('Subject'), t('Status'), t('MailChimp List'), t('MailChimp Template'), t('Created'), t('Actions')),
+      '#empty' => '',
+    );
+
+    $campaigns = mailchimp_campaign_load_multiple();
+    $templates = mailchimp_campaign_list_templates();
+
+    foreach ($campaigns as $campaign) {
+      $archive_url = Url::fromUri($campaign->mc_data['archive_url']);
+      $campaign_url = Url::fromRoute('mailchimp_campaign.view', array('campaign_id' => $campaign->mc_campaign_id));
+      $list_url = Url::fromUri('https://admin.mailchimp.com/lists/dashboard/overview?id=' . $campaign->list['web_id']);
+
+      $actions = array(
+        \Drupal::l(t('View Archive'), $archive_url),
+        \Drupal::l(t('View'), $campaign_url),
+      );
+
+      $content['campaigns_table'][$campaign->mc_campaign_id]['title'] = array(
+        '#markup' => \Drupal::l($campaign->label(), $campaign_url),
+      );
+
+      $content['campaigns_table'][$campaign->mc_campaign_id]['subject'] = array(
+        '#markup' => $campaign->mc_data['subject'],
+      );
+
+      $content['campaigns_table'][$campaign->mc_campaign_id]['status'] = array(
+        '#markup' => $campaign->mc_data['status'],
+      );
+
+      $content['campaigns_table'][$campaign->mc_campaign_id]['list'] = array(
+        '#markup' => \Drupal::l($campaign->list['name'], $list_url, array(
+            'attributes' => array('target' => '_blank'),
+          )),
+      );
+
+      $content['campaigns_table'][$campaign->mc_campaign_id]['template'] = array(
+        '#markup' => isset($templates[$campaign->mc_data['template_id']]) ? $templates[$campaign->mc_data['template_id']]['name'] : '',
+      );
+
+      $content['campaigns_table'][$campaign->mc_campaign_id]['created'] = array(
+        '#markup' => $campaign->mc_data['create_time'],
+      );
+
+      $content['campaigns_table'][$campaign->mc_campaign_id]['actions'] = array(
+        '#markup' => implode(' | ', $actions),
+      );
+    }
+
+    return $content;
+  }
+
+  /**
+   * View a MailChimp campaign
+   *
+   * @param int $campaign_id
+   *   The ID of the campaign to view.
+   *
+   * @return array
+   *   Renderable array of page content.
+   */
+  public function view($campaign_id) {
+    $content = array();
+
     return $content;
   }
 
