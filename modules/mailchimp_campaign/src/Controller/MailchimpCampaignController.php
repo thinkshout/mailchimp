ff8bc0a524866c168ae0cf3e1794a0d9a0b080d2
Gabe Carleton-Barnes <gabe@thinkshout.com>
2015-04-30 14:23:39 -0700

    Locate template name fol listing on campaign overview.

--- modules/mailchimp_campaign/src/Controller/MailchimpCampaignController.php
+++ modules/mailchimp_campaign/src/Controller/MailchimpCampaignController.php
@@ -65,10 +65,37 @@ class MailchimpCampaignController extends ControllerBase {
           )),
       );
 
-      $content['campaigns_table'][$campaign_id]['template'] = array(
-        '#markup' => isset($templates[$campaign->mc_data['template_id']]) ? $templates[$campaign->mc_data['template_id']]['name'] : '',
       );
 
+      if (empty($campaign->mc_data['template_id'])) {
+        $content['campaigns_table'][$campaign_id]['template'] = array(
+          '#markup' => "-- none --",
+        );
+      }
+      else {
+        $template_url = Url::fromUri('https://admin.mailchimp.com/templates/edit?id=' . $campaign->mc_data['template_id'], array('attributes' => array('target' => '_blank')));
+        $category = FALSE;
+        // Templates are grouped into categories, so we go hunting for our
+        // template ID in each category.
+        foreach($templates as $category_name => $template_category) {
+          if (isset($template_category[$campaign->mc_data['template_id']])) {
+            $category = $category_name;
+            break;
+          }
+        }
+        if ($category) {
+          $content['campaigns_table'][$campaign_id]['template'] = array(
+            '#markup' => \Drupal::l($template_category[$campaign->mc_data['template_id']]['name'], $template_url)
+          );
+        }
+        else {
+          $content['campaigns_table'][$campaign_id]['template'] = array(
+            '#markup' => '-- template ' .
+                \Drupal::l($campaign->mc_data['template_id'], $template_url, array('attributes' => array('target' => '_blank')))
+                . ' not found --',
+          );
+        }
+      }
       $content['campaigns_table'][$campaign_id]['created'] = array(
         '#markup' => $campaign->mc_data['create_time'],
       );
