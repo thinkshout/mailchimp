19bf8cffcf101be75f3770ba4a8673b2a1de9579
amyvs <amy@thinkshout.com>
2015-04-30 16:30:58 -0700

    Adjust  variable to get 'system.site' configurations, set condition for when '->mc_data' is empty, and return default strings for site name and email if condition is false.

--- modules/mailchimp_campaign/src/Form/MailchimpCampaignForm.php
+++ modules/mailchimp_campaign/src/Form/MailchimpCampaignForm.php
@@ -44,7 +44,7 @@ class MailchimpCampaignForm extends ContentEntityForm {
    */
   public function form(array $form, FormStateInterface $form_state) {
     $form = parent::form($form, $form_state);
-    $config = \Drupal::config('mailchimp.settings');
+    $config = \Drupal::config('system.site');
 
     // Attach campaign JS and CSS.
     $form['#attached']['library'][] = 'mailchimp_campaign/campaign-form';
@@ -114,7 +114,7 @@ class MailchimpCampaignForm extends ContentEntityForm {
       '#type' => 'textfield',
       '#title' => t('From Email'),
       '#description' => t('the From: email address for your campaign message.'),
-      '#default_value' => ($campaign) ? $campaign->mc_data['from_email'] : $config->get('site_mail'),
+      '#default_value' => (!empty($campaign->mc_data)) ? $campaign->mc_data['from_email'] : $config->get('mail'),
       '#size' => 40,
       '#maxlength' => 255,
       '#required' => TRUE,
@@ -123,7 +123,7 @@ class MailchimpCampaignForm extends ContentEntityForm {
       '#type' => 'textfield',
       '#title' => t('From Name'),
       '#description' => t('the From: name for your campaign message (not an email address)'),
-      '#default_value' => ($campaign) ? $campaign->mc_data['from_name'] : $config->get('site_name'),
+      '#default_value' => (!empty($campaign->mc_data)) ? $campaign->mc_data['from_name'] : $config->get('name'),
       '#size' => 40,
       '#maxlength' => 255,
       '#required' => TRUE,
