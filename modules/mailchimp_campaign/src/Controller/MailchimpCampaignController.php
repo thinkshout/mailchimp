643e5a1f3d067602d5ce2e720c8d7c3270ce1229
amyvs <amy@thinkshout.com>
2016-04-05 15:03:02 -0700

    Make sure the MailchimpCampaignController pulls in the correct data.

--- modules/mailchimp_campaign/src/Controller/MailchimpCampaignController.php
+++ modules/mailchimp_campaign/src/Controller/MailchimpCampaignController.php
@@ -57,11 +57,11 @@ class MailchimpCampaignController extends ControllerBase {
       );
 
       $content['campaigns_table'][$campaign_id]['title'] = array(
-        '#markup' => \Drupal::l($campaign->mc_data->title, $campaign_url),
+        '#markup' => \Drupal::l($campaign->mc_data->settings->title, $campaign_url),
       );
 
       $content['campaigns_table'][$campaign_id]['subject'] = array(
-        '#markup' => $campaign->mc_data->subject,
+        '#markup' => $campaign->mc_data->settings->subject_line,
       );
 
       $content['campaigns_table'][$campaign_id]['status'] = array(
@@ -72,32 +72,32 @@ class MailchimpCampaignController extends ControllerBase {
         '#markup' => \Drupal::l($campaign->list->name, $list_url),
       );
 
-      if (empty($campaign->mc_data->template_id)) {
+      if (empty($campaign->mc_data->settings->template_id)) {
         $content['campaigns_table'][$campaign_id]['template'] = array(
           '#markup' => "-- none --",
         );
       }
       else {
-        $template_url = Url::fromUri('https://admin.mailchimp.com/templates/edit?id=' . $campaign->mc_data->template_id, array('attributes' => array('target' => '_blank')));
+        $template_url = Url::fromUri('https://admin.mailchimp.com/templates/edit?id=' . $campaign->mc_data->settings->template_id, array('attributes' => array('target' => '_blank')));
         $category = FALSE;
         // Templates are grouped into categories, so we go hunting for our
         // template ID in each category.
         $template_category = array();
         foreach($templates as $category_name => $template_category) {
-          if (isset($template_category[$campaign->mc_data->template_id])) {
+          if (isset($template_category[$campaign->mc_data->settings->template_id])) {
             $category = $category_name;
             break;
           }
         }
         if ($category) {
           $content['campaigns_table'][$campaign_id]['template'] = array(
-            '#markup' => \Drupal::l($template_category[$campaign->mc_data->template_id]['name'], $template_url)
+            '#markup' => \Drupal::l($template_category[$campaign->mc_data->settings->template_id]->name, $template_url)
           );
         }
         else {
           $content['campaigns_table'][$campaign_id]['template'] = array(
             '#markup' => '-- template ' .
-                \Drupal::l($campaign->mc_data->template_id, $template_url, array('attributes' => array('target' => '_blank')))
+                \Drupal::l($campaign->mc_data->settings->template_id, $template_url, array('attributes' => array('target' => '_blank')))
                 . ' not found --',
           );
         }
