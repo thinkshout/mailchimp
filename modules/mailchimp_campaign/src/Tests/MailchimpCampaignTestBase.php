5c5d93930f1890e92323c605dda8ab4bdbbf2853
amyvs <amy@thinkshout.com>
2016-04-14 10:52:47 -0700

    Include library paths for the tests, so they can communicate.

--- modules/mailchimp_campaign/src/Tests/MailchimpCampaignTestBase.php
+++ modules/mailchimp_campaign/src/Tests/MailchimpCampaignTestBase.php
@@ -10,6 +10,14 @@ namespace Drupal\mailchimp_campaign\Tests;
 use Drupal\mailchimp_campaign_test\MailchimpCampaignConfigOverrider;
 use Drupal\simpletest\WebTestBase;
 
+$path = drupal_get_path('module', 'mailchimp');
+
+include_once $path . "/lib/mailchimp-api-php/tests/src/Client.php";
+include_once $path . "/lib/mailchimp-api-php/tests/src/Mailchimp.php";
+include_once $path . "/lib/mailchimp-api-php/tests/src/Response.php";
+include $path . "/lib/mailchimp-api-php/tests/src/MailchimpCampaigns.php";
+
+
 /**
  * Sets up MailChimp Campaign module tests.
  */
--- modules/mailchimp_lists/src/Tests/MailchimpListsTestBase.php
+++ modules/mailchimp_lists/src/Tests/MailchimpListsTestBase.php
@@ -10,6 +10,13 @@ namespace Drupal\mailchimp_lists\Tests;
 use Drupal\simpletest\WebTestBase;
 use Drupal\mailchimp_lists_test\MailchimpListsConfigOverrider;
 
+$path = drupal_get_path('module', 'mailchimp');
+
+include_once $path . "/lib/mailchimp-api-php/tests/src/Client.php";
+include_once $path . "/lib/mailchimp-api-php/tests/src/Mailchimp.php";
+include_once $path . "/lib/mailchimp-api-php/tests/src/Response.php";
+include $path . "/lib/mailchimp-api-php/tests/src/MailchimpLists.php";
+
 /**
  * Sets up MailChimp Lists module tests.
  */
